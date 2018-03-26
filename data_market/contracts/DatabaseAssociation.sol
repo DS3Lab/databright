870766c877683176186077a6126eac26ec4ec1bf
Florian Chlan <florian@chlan.net>
2018-03-26 15:48:42 +0200

    Removed obsolete ether function

--- data_market/contracts/DatabaseAssociation.sol
+++ data_market/contracts/DatabaseAssociation.sol
@@ -145,31 +145,6 @@ contract DatabaseAssociation is Ownable, tokenRecipient {
 
         return proposalID;
     }
-
-    /**
-     * Add proposal in Ether
-     *
-     * Propose to send `etherAmount` ether to `beneficiary` for `jobDescription`. `transactionBytecode ? Contains : Does not contain` code.
-     * This is a convenience function to use if the amount to be given is in round number of ether units.
-     *
-     * @param beneficiary who to send the ether to
-     * @param etherAmount amount of ether to send
-     * @param jobDescription Description of job
-     * @param transactionBytecode bytecode of transaction
-     */
-    function newProposalInEther(
-        address beneficiary,
-        uint etherAmount,
-        string jobDescription,
-        bytes transactionBytecode,
-        string argument,
-        address curator
-    ) public
-        //onlyShareholders
-        returns (uint proposalID)
-    {
-        return newProposal(beneficiary, etherAmount * 1 ether, jobDescription, transactionBytecode, argument, curator, 0);
-    }
     
     /**
       * Add new database
