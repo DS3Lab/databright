c8e48f6488ecc8ed34df50446cfb129a6ac40c1a
Florian Chlan <florian@chlan.net>
2018-03-29 13:33:50 +0200

    Adapted DB assoc constructor call

--- data_market/migrations/2_deploy_token.js
+++ data_market/migrations/2_deploy_token.js
@@ -4,6 +4,6 @@ var DatabaseAssociation = artifacts.require("DatabaseAssociation");
 module.exports = function(deployer) {
   // first deploy CuratorToken, then use its address to deploy the Database Association
   deployer.deploy(CuratorToken).then(function(){
-    return deployer.deploy(DatabaseAssociation, CuratorToken.address, 10, 2, 1, 1000)
+    return deployer.deploy(DatabaseAssociation, 10, 2, 1000)
   });
 }
