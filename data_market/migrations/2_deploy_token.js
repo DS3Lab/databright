30b1687852643aa93064eac1c1ba470aa64b69d4
Florian Chlan <florian@chlan.net>
2018-04-14 11:00:39 +0200

    Removed obsolete deploy script

--- data_market/migrations/2_deploy_token.js
+++ data_market/migrations/2_deploy_token.js
@@ -2,8 +2,5 @@ var CuratorToken = artifacts.require("CuratorToken");
 var DatabaseAssociation = artifacts.require("DatabaseAssociation");
 
 module.exports = function(deployer) {
-  // first deploy CuratorToken, then use its address to deploy the Database Association
-  deployer.deploy(CuratorToken).then(function(){
-    return deployer.deploy(DatabaseAssociation, 10, 2, 1000)
-  });
+  deployer.deploy(DatabaseAssociation, 2, 1000);
 }
