b3fee501cca1c5b1e2dfeb8d15dbd2a5f8f79e92
Florian Chlan <florian@chlan.net>
2018-05-20 16:32:55 +0200

    Added option to replay past events (using filters); Print account and balance at startup; Added generic code to handle past events

--- model_market/src/main.rs
+++ model_market/src/main.rs
@@ -5,7 +5,7 @@ extern crate web3;
 
 use ini::Ini;
 use std::collections::HashMap;
-use web3::contract::{Contract, Options};
+use web3::contract::Contract;
 use web3::types::{Address, FilterBuilder, BlockNumber, H256};
 use web3::futures::{Future, Stream};
 use std::time;
@@ -20,9 +20,15 @@ fn main() {
     
     let web3_section = conf.section(Some("Web3".to_owned())).unwrap();
     let ws_url = web3_section.get("websocket_transport_url").unwrap();
-    let last_processed_block_id = web3_section.get("last_processed_block_id").unwrap();
+    let replay_past_events = {
+        let replay_string = web3_section.get("replay_past_events").unwrap();
+        match replay_string.parse::<bool>() {
+            Ok(b) => b,
+            Err(_) => {println!("Couldn't parse replay_past_events from configuration. Replaying events from the past.."); true },
+        }
+    };
 
-    let contract_path = "../data_market/build/";
+    let last_processed_block_id = web3_section.get("last_processed_block_id").unwrap();
 
     // Populate topic hashmap
     let mut topics: HashMap<(&str, String), H256> = HashMap::new();
@@ -45,41 +51,65 @@ fn main() {
     let transp = web3::transports::WebSocket::with_event_loop(ws_url, &handle).unwrap();
     let web3 = web3::Web3::new(transp);
 
+    // Print accounts and balance to check if websocket connection works
+    let accounts = web3.eth().accounts().map(|accounts| {
+        println!("Accounts: {:?}", accounts);
+        accounts[0]
+	});
+
+	let my_acc = event_loop.run(accounts).unwrap();
+	let bal = web3.eth().balance(my_acc, None).map(|balance| {
+		println!("Balance of {}: {}", my_acc, balance);
+	});
+    event_loop.run(bal).unwrap();
+
+    let database_association_contract = Contract::from_json(
+        web3.eth(),
+        database_association_address,
+        include_bytes!("../../data_market/build/DatabaseAssociation.abi"),
+    ).unwrap();
+
     // Retrieve logs since last processed block
-    let desired_topic = topics.get(&("DatabaseAssociation", "ProposalAdded".into())).unwrap();
-    let from_block = if last_processed_block_id.is_empty() { 
-            BlockNumber::Earliest
-        } else {
-            match last_processed_block_id.parse::<u64>() {
-                Ok(n) => BlockNumber::Number(n),
-                Err(err) => {println!("Couldn't parse last_processed_block_id from configuration. Starting from earliest block..."); BlockNumber::Earliest },
-            }
-        };
 
-    let filter = FilterBuilder::default()
-        .address(vec![database_association_address])
-        .from_block(from_block)
-        .to_block(BlockNumber::Latest)
-        .topics(
-            Some(vec![
-                *desired_topic,
-            ]),
-            None,
-            None,
-            None,
-        )
-        .build();
+    // TODO To filter for specific events, like the Voted-event of DatabaseAssociation:
+    // let desired_topics = Some(vec![*topics.get(&("DatabaseAssociation", "Voted".into())).unwrap()]);
+    let desired_topics = None;
 
-    let event_future = web3.eth_filter()
-        .create_logs_filter(filter)
-        .then(|filter| {
-            filter
-                .unwrap()
-                .stream(time::Duration::from_secs(0))
-                .for_each(|log| {
-                    println!("got log: {:?}", log);
-                    Ok(())
-                })
-        })
-        .map_err(|_| ());
+    if replay_past_events {
+        let from_block = if last_processed_block_id.is_empty() {
+                BlockNumber::Earliest
+            } else {
+                match last_processed_block_id.parse::<u64>() {
+                    Ok(n) => BlockNumber::Number(n),
+                    Err(_) => {println!("Couldn't parse last_processed_block_id from configuration. Starting from earliest block..."); BlockNumber::Earliest },
+                }
+            };
+
+        let filter = FilterBuilder::default()
+            .address(vec![database_association_contract.address()])
+            .from_block(from_block)
+            .to_block(BlockNumber::Latest)
+            .topics(
+                desired_topics,
+                None,
+                None,
+                None,
+            )
+            .build();
+
+        let event_future = web3.eth_filter()
+            .create_logs_filter(filter)
+            .then(|filter| {
+                filter
+                    .unwrap()
+                    .stream(time::Duration::from_secs(0))
+                    .for_each(|log| {
+                        // TODO Handle filtered log here
+                        Ok(())
+                    })
+            })
+            .map_err(|_| ());
+
+        event_loop.run(event_future);
+    }
 }
