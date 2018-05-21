ec99b18c98b7e5b7e1aa65287c35d728a7f0229f
Florian Chlan <florian@chlan.net>
2018-05-21 11:09:43 +0200

    Added logging

--- model_market/src/main.rs
+++ model_market/src/main.rs
@@ -2,22 +2,28 @@ extern crate ini;
 extern crate csv;
 extern crate tokio_core;
 extern crate web3;
+#[macro_use] extern crate log;
+extern crate env_logger;
 
 use ini::Ini;
 use std::collections::HashMap;
 use web3::contract::Contract;
 use web3::types::{Address, FilterBuilder, BlockNumber, H256};
 use web3::futures::{Future, Stream};
-use std::time;
 
 fn main() {
 
+    env_logger::init();
+
     // Extract configuration from config.ini
+    info!("Extracting configuration from config.ini..");
     let conf = Ini::load_from_file("config.ini").unwrap();
     let contracts_section = conf.section(Some("Contracts".to_owned())).unwrap();
     let contracts = contracts_section.get("contracts").unwrap();
     let database_association_address: Address = contracts_section.get("DatabaseAssociation").unwrap().parse().unwrap();
-    
+    debug!("Contracts to use: {}", contracts);
+    debug!("DatabaseAssociation to use: {}", database_association_address);
+
     let web3_section = conf.section(Some("Web3".to_owned())).unwrap();
     let ws_url = web3_section.get("websocket_transport_url").unwrap();
     let replay_past_events = {
@@ -31,6 +37,7 @@ fn main() {
     let last_processed_block_id = web3_section.get("last_processed_block_id").unwrap();
 
     // Populate topic hashmap
+    info!("Loading topic hashes from ../data_marketr/build/*.topic files..");
     let mut topics: HashMap<(&str, String), H256> = HashMap::new();
     for contract in contracts.split(",") {
         let mut rdr = csv::Reader::from_path(format!("../data_market/build/{}.topic", contract)).unwrap();
@@ -46,6 +53,7 @@ fn main() {
 
 
     // Connect to ethereum node
+    info!("Connecting to ethereum node at {}", ws_url);
     let mut event_loop = tokio_core::reactor::Core::new().unwrap();
     let handle = event_loop.handle();
     let transp = web3::transports::WebSocket::with_event_loop(ws_url, &handle).unwrap();
@@ -53,13 +61,13 @@ fn main() {
 
     // Print accounts and balance to check if websocket connection works
     let accounts = web3.eth().accounts().map(|accounts| {
-        println!("Accounts: {:?}", accounts);
+        debug!("Accounts on node: {:?}", accounts);
         accounts[0]
 	});
 
-	let my_acc = event_loop.run(accounts).unwrap();
-	let bal = web3.eth().balance(my_acc, None).map(|balance| {
-		println!("Balance of {}: {}", my_acc, balance);
+	let accs = event_loop.run(accounts).unwrap();
+	let bal = web3.eth().balance(accs, None).map(|balance| {
+		debug!("Balance of {}: {}", accs, balance);
 	});
     event_loop.run(bal).unwrap();
 
@@ -72,23 +80,32 @@ fn main() {
     // Retrieve logs since last processed block
 
     // TODO To filter for specific events:
-    //let desired_topics = Some(
+    //let desired_topics: std::option::Option<std::vec::Vec<web3::types::H256>> = Some(
     //    vec![*topics.get(&("DatabaseAssociation", "Voted".into())).unwrap(),
     //         *topics.get(&("DatabaseAssociation", "ProposalAdded".into())).unwrap()
     //    ]);
-    let desired_topics = None;
+    let desired_topics: std::option::Option<std::vec::Vec<web3::types::H256>> = None;
+    let num_events = match desired_topics {
+        Some(ref vec) => vec.len(),
+        None => 0
+    };
+    if num_events == 0 {
+        info!("Listening for all events");
+    } else {
+        info!("Listening for {} events", num_events);
+    }
 
     if replay_past_events {
-
+        info!("replay_past_events is true. Will replay events from the past..");
         let from_block = if last_processed_block_id.is_empty() {
                 BlockNumber::Earliest
             } else {
                 match last_processed_block_id.parse::<u64>() {
                     Ok(n) => BlockNumber::Number(n),
-                    Err(_) => {println!("Couldn't parse last_processed_block_id from configuration. Starting from earliest block..."); BlockNumber::Earliest },
+                    Err(_) => {warn!("Couldn't parse last_processed_block_id from configuration. Starting from earliest block..."); BlockNumber::Earliest },
                 }
             };
-
+        debug!("Replaying events from {:?} to latest block", from_block);
         let filter = FilterBuilder::default()
             .address(vec![database_association_contract.address()])
             .from_block(from_block)
@@ -107,6 +124,7 @@ fn main() {
                 let res = filter.logs().and_then(|logs| {
                     for log in logs {
                         // TODO Handle filtered log here
+                        debug!("Log: {:?}", log);
                     }
                     Ok(())
                 });
@@ -115,5 +133,6 @@ fn main() {
             .map_err(|_| ());
 
         event_loop.run(event_future);
+        debug!("Finished replay of events");
     }
 }
