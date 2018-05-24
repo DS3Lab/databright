e28a7a63e2feaca816a138d75bb59e97a0c88fd0
Florian Chlan <florian@chlan.net>
2018-05-24 14:04:05 +0200

    Implemented knn_shapley calculation for backend

--- model_market/src/knn_shapley.rs
+++ model_market/src/knn_shapley.rs
@@ -14,30 +14,35 @@ mod knn_shapley {
                                 .collect();
     }
 
-    fn shapleys_from_distances(distances: Vector<f64>, training_labels: Vector<i32>, test_label: i32, k: i8) -> Vec<f64> {
-        let num_training_labels = distances.size();
-        let shapleys = vec![0; num_training_labels];
+    fn shapleys_from_distances(distances: Vec<f64>, training_labels: &Vector<u32>, test_label: u32, k: usize) -> Vector<f64> {
+        let num_training_labels = distances.len();
+        let mut shapleys = vec![0.0; num_training_labels];
         let mut training_idx = num_training_labels;
-        let mut lastvalue = 0;
-        let mut lastkey = -1;
+        let mut lastvalue = 0.0;
+        let mut lastkey = None;
 
-        let sorted_distances = distances.iter().enumerate().sort_by(|(idx_a, val_a),(idx_b, val_b)| val_b.partial_cmp(val_a).unwrap());
-        for (key, dist) in sorted_distances {
-            if training_idx == num_training_labels {
-                lastvalue = 0;
-            } else {
-                let numerator = ((training_labels[key] == test_label) as f64) - ((training_labels[lastkey] == test_label) as f64) * (min(k-1, training_idx-1) + 1);
-                let denominator = k * training_idx;
+        let mut index_vec: Vec<usize> = (0..num_training_labels).collect();
+        index_vec
+            .sort_by(|idx_a, idx_b| distances[*idx_b]
+                                    .partial_cmp(&distances[*idx_a])
+                                    .unwrap());
+        
+        for key in index_vec.iter() {
+            if training_idx != num_training_labels {
+                let numerator = (((training_labels[*key] == test_label) as u8) 
+                                - ((training_labels[lastkey.unwrap()] == test_label) as u8)) as f64
+                                * ((min(k-1, training_idx-1) + 1) as f64);
+                let denominator = (k * training_idx) as f64;
                 lastvalue += numerator/denominator;
             }
 
-            lastkey = key;
-            shapleys[key] = lastvalue;
+            lastkey = Some(*key);
+            shapleys[*key] = lastvalue;
         }
 
-        return shapleys;
+        return Vector::new(shapleys);
     }
-    pub fn calculate_knn_shapleys(training_features: Matrix<f64>, training_labels: Vector<i32> , test_features: Matrix<f64>, test_labels: Vector<i32>, k: i8) {
+    pub fn calculate_knn_shapleys(training_features: &Matrix<f64>, training_labels: &Vector<u32> , test_features: &Matrix<f64>, test_labels: &Vector<u32>, k: usize) {
         assert_eq!(training_features.rows(), training_labels.size());
         assert_eq!(test_features.rows(), test_labels.size());
         assert_eq!(training_features.cols(), test_features.cols());
@@ -45,6 +50,6 @@ mod knn_shapley {
         test_features.iter_rows().enumerate()
                      .map(|(idx, row)| (idx, distance_from_sample(&training_features, &Vector::new(row)) ))
                      .map(|(idx, dist)| shapleys_from_distances(dist, training_labels, test_labels[idx], k))
-                     .fold(vec![0,training_labels.size()], |sum_vec, shapleys| sum_vec + shapleys);
+                     .fold(Vector::new(vec![0.0;training_labels.size()]), |sum_vec, shapleys| sum_vec + shapleys);
     }
 }
