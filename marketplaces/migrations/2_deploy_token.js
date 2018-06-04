d836ac6c00e3fe9157bcc4525b577577d428b666
Florian Chlan <florian@chlan.net>
2018-06-04 19:03:54 +0200

    Fixed contract deployment of DBAssoc and ModelMarket

--- marketplaces/migrations/2_deploy_token.js
+++ marketplaces/migrations/2_deploy_token.js
@@ -3,10 +3,12 @@ var DatabaseAssociation = artifacts.require("DatabaseAssociation");
 var ModelMarket = artifacts.require("ModelMarket");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(DatabaseAssociation, 2, 10)
-  .then( ()=> 
-    deployer.deploy(ModelMarket, DatabaseAssociation.deployed().address, accounts[0]))
-  .then( ()=>
-    DatabaseAssociation.deployed().associateModelMarket(ModelMarket.deployed().address));
+  deployer.deploy(DatabaseAssociation, 2, 10).then(function() {
+    return deployer.deploy(ModelMarket, DatabaseAssociation.address, accounts[0]);
+  }).then(function() {
+    return DatabaseAssociation.deployed();
+  }).then(function(dba) {
+    dba.associateModelMarket(ModelMarket.address);
+  });
 }
 
